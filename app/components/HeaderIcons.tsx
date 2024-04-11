b36410dfc4e76bf4fefc94967ba1296ab6be933b
Md Christien <mdchristien12@gmail.com>
2024-04-11 12:56:54 +0100

    set up next auth

--- app/components/HeaderIcons.tsx
+++ app/components/HeaderIcons.tsx
@@ -8,19 +8,18 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/24/outline";
 
-
 const HeaderIcons = () => {
-  const [active, setActive] = useState(true);
+  const [activeIndex, setActiveIndex] = useState(0);
 
-  const iconStyles = `h-5 text-gray-500 text-center sm:7 mx-auto  group-hover:text-blue-500`;
+  const iconStyles = `h-5 text-gray-500 text-center sm:7 mx-auto  group-hover:text-blue-500 ${
+    activeIndex === 0 && "text-blue-500"
+  }`;
   const iconContainerStyles =
     "flex items-center cursor-pointer md:px-8 hover:bg-gray-100 rounded-2xl sm:h-14 active:border-b-2 active:border-blue-500 group";
 
   const icons = [
     {
-      icon: (
-        <HomeIcon className={`${iconStyles} ${active && "text-blue-500"}`} />
-      ),
+      icon: <HomeIcon className={`${iconStyles}`} />,
       containerStyles: iconContainerStyles,
     },
     {
@@ -44,7 +43,11 @@ const HeaderIcons = () => {
   return (
     <div className="flex gap-6 md:gap-2">
       {icons.map((icon, index) => (
-        <div key={index} className={`${icon.containerStyles}`}>
+        <div
+          key={index}
+          className={`${icon.containerStyles}`}
+          onClick={() => setActiveIndex(index)}
+        >
           {icon.icon}
         </div>
       ))}
